771a59bec70baa66739552f263af0505dd4a5956
Marianna Coppola <marianna_cm@libero.it>
2021-12-11 13:17:13 +0100

    Ex11.1

--- src/app/beer.service.ts
+++ src/app/beer.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
+import { PRODUCTS } from './shared/mock-data';
+import { IProduct } from './shared/product';
+import { find } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
+  private products: IProduct[] = PRODUCTS;
+  private beers$: Observable<IProduct> = from(this.products);
 
   constructor() { }
-}
+
+  getBeers() : Observable<IProduct> {
+    return this.beers$;
+  }
+
+  getBeerById(id: number) {
+    let myBeer: IProduct | undefined;
+    this.getBeers()
+      .pipe(find(product => product.id === id))
+      .subscribe((beer) => myBeer = beer);
+    return myBeer;
+    }
+
+  //all'inizio non riuscivo a stamparmi il result in console quindi smanettando un pò ero arrivata a questo sotto,
+  //quindi te lo lascio però era merito di vsc :)
+
+  /* private products$: Observable<IProduct[]> = of(PRODUCTS);
+  beer$: IProduct | undefined;
+
+  constructor() { }
+
+  getBeers() {
+    return this.products$;
+  }
+
+  getBeerById(id: number) {
+    this.getBeers().subscribe(products => this.beer$ = products.find( product => product.id === id)); //vsc suggerisce ed importa automaticamente beer$
+    return this.beer$;
+  } */
+  }
